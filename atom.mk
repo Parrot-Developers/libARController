a3aeda3fb4f24b338367d3695d6d599e078f4d3b
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-01-22 17:24:14 +0100

    [BUILD] Do not use autotools anymore, just alchemy build
    Change-Id: I671d59e9cbb86ed3a3f494f7ac5552a808e3d4c7
    Reviewed-on: https://gerrit.parrot.biz/28566
    Reviewed-by: Nicolas Christe <nicolas.christe@parrot.com>

--- atom.mk
+++ atom.mk
@@ -2,54 +2,56 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CATEGORY_PATH := dragon/libs
 LOCAL_MODULE := libARController
 LOCAL_DESCRIPTION := ARSDK Controller
+LOCAL_CATEGORY_PATH := dragon/libs
 
-LOCAL_LIBRARIES := ARSDKBuildUtils libARSAL libARDiscovery libARCommands libARUtils libARStream libARStream2 uthash
+LOCAL_MODULE_FILENAME := libarcontroller.so
 
-# Copy in build dir so bootstrap files are generated in build dir
-LOCAL_AUTOTOOLS_COPY_TO_BUILD_DIR := 1
+LOCAL_LIBRARIES := \
+	libARSAL \
+	libARDiscovery \
+	libARCommands \
+	libARUtils \
+	libARStream \
+	libARStream2 \
+	libARNetworkAL \
+	libARNetwork \
+	uthash \
+	json
 
-# Configure script is not at the root
-LOCAL_AUTOTOOLS_CONFIGURE_SCRIPT := Build/configure
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/Includes \
+	$(LOCAL_PATH)/gen/Includes \
+	$(LOCAL_PATH)/Sources
 
-# Autotools variable
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
-	--with-libARSALInstallDir="" \
-	--with-libARNetworkALInstallDir="" \
-	--with-libARNetworkInstallDir="" \
-	--with-libARDiscoveryInstallDir="" \
-	--with-libARCommandsInstallDir="" \
-	--with-libARStreamInstallDir="" \
-	--with-libARStream2InstallDir="" \
-	--with-uthashInstallDir="" \
-	--with-jsonInstallDir=""
+LOCAL_SRC_FILES := \
+	Sources/ARCONTROLLER_Dictionary.c \
+	Sources/ARCONTROLLER_Frame.c \
+	Sources/ARCONTROLLER_Network.c \
+	Sources/ARCONTROLLER_Stream.c \
+	Sources/ARCONTROLLER_StreamPool.c \
+	Sources/ARCONTROLLER_StreamQueue.c \
+	Sources/ARCONTROLLER_Stream1.c \
+	Sources/ARCONTROLLER_Stream2.c \
+	gen/Sources/ARCONTROLLER_Device.c \
+	gen/Sources/ARCONTROLLER_DICTIONARY_Key.c \
+	gen/Sources/ARCONTROLLER_Error.c \
+	gen/Sources/ARCONTROLLER_Feature.c
 
-ifeq ("$(TARGET_OS_FLAVOUR)","android")
+LOCAL_INSTALL_HEADERS := \
+	Includes/libARController/ARController.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_Dictionary.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_Error.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_Frame.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_Network.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_Stream.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_Stream1.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_Stream2.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_StreamPool.h:usr/include/libARController/ \
+	Includes/libARController/ARCONTROLLER_StreamQueue.h:usr/include/libARController/ \
+	gen/Includes/libARController/ARCONTROLLER_Device.h:usr/include/libARController/ \
+	gen/Includes/libARController/ARCONTROLLER_DICTIONARY_Key.h:usr/include/libARController/ \
+	gen/Includes/libARController/ARCONTROLLER_Feature.h:usr/include/libARController/
 
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--disable-static \
-	--enable-shared \
-	--disable-so-version
-
-else ifneq ($(filter iphoneos iphonesimulator, $(TARGET_OS_FLAVOUR)),)
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--enable-static \
-	--disable-shared \
-	CCASFLAGS=" -x assembler-with-cpp -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJCFLAGS=" -x objective-c -fobjc-arc -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJC="$(TARGET_CC)" \
-	CFLAGS=" -std=gnu99 -x c $(TARGET_GLOBAL_CFLAGS)"
-
-endif
-
-define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
-	$(Q) cd $(PRIVATE_SRC_DIR)/Build && ./bootstrap
-endef
-
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/Includes
-LOCAL_EXPORT_LDLIBS := -larcontroller
-
-include $(BUILD_AUTOTOOLS)
+include $(BUILD_LIBRARY)
